8f03207b456dc77c849e91479e466ac87097745e
mvntr <marcelovieirantr@gmail.com>
2023-08-21 16:43:25 -0300

    teste dos packages

---
+++ src/listas/testedois.java
@@ -0,0 +1,10 @@
+package listas;
+
+public class testedois {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/matrizes/testetres.java
@@ -0,0 +1,10 @@
+package matrizes;
+
+public class testetres {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
